a0d82da5010507d735e76478f34600d0c9382cb7
Leon Deng <wi18169@lehre.dhbw-stuttgart.de>
2022-10-06 09:06:55 +0200

    initial commit and import data

---
+++ implementation.r
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+ab_test <- read.csv("data.csv")
+
+
+# Did it increase revenue or interactions?
+
+# Is it relevant for everyone or just for a specific target group? Difference between channels, customers?
+
+# By how much in terms of revenue increase would this campaign be effective
+
+# Hint: Include Error-bars of standard error / deviation
+
+# Submit to scheibehenne@kit.edu, use a .ppt and put your names on it
