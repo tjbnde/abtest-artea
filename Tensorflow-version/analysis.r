93877f6020b3940c89a557439c6525e67c23fad6
Tobias Jansen <tobias.jansen@sap.com>
2022-10-08 13:08:50 +0200

    Tensorflow analysis

--- Tensorflow-version/analysis.r
+++ Tensorflow-version/analysis.r
@@ -8,15 +8,46 @@ data <- read.csv("outputdata.csv")
 mean(data$trans_after)
 
 
-users_to_send_coupon = 
-users_to_not_send_coupon = 
-  
-  
-data$test_coupon[data$prediction_using_neural_network > 0.3]
-data$test_coupon[data$prediction_using_neural_network > 0.3]
+
+mean(data$trans_after[data$prediction_using_neural_network < 0.3])
 
 
-data$trans_after[data$test_coupon == 0]
+
+# Wir müssen schauen, ob wenn die Gruppe mit Coupon höher predicted wird, auch der tatsächliche Kauf höher ist (und umgekehrt)
+mean(data$trans_after[data$test_coupon == 0 & data$prediction_using_neural_network > 0.3])
+mean(data$trans_after[data$test_coupon == 1 & data$prediction_using_neural_network > 0.3])
+mean(data$trans_after[data$test_coupon == 0 & data$prediction_using_neural_network < 0.3])
+mean(data$trans_after[data$test_coupon == 1 & data$prediction_using_neural_network < 0.3])
+
+
+
+
+
+
+with_coupon = data[data$test_coupon == 0]
+  
+
+reference = sum(data$trans_after[data$test_coupon == 0])
+
+sum(data$trans_after[data$test_coupon == 1 & data$prediction_using_neural_network > 0.3])
+
+
+sum(data$trans_after[data$test_coupon == 0 & data$prediction_using_neural_network < 0.3])
+
+
+sum(data$trans_after[data$prediction_using_neural_network < 0.3])
+
+
+
+sum(data$trans_after[data$test_coupon == 0 & data$prediction_using_neural_network > 0.3])
+sum(data$trans_after[data$test_coupon == 1 & data$prediction_using_neural_network > 0.3])
+sum(data$trans_after[data$test_coupon == 0 & data$prediction_using_neural_network < 0.3])
+sum(data$trans_after[data$test_coupon == 1 & data$prediction_using_neural_network < 0.3])
+
+
+trans_after_for_users_without_coupon = data$trans_after[data$test_coupon == 0]
+
+trans_after_for_users_wit_coupon = data$trans_after[data$test_coupon == 1]
 
 mean(data$trans_after[data$test_coupon == 0])
 mean(data$trans_after[data$test_coupon == 1])
