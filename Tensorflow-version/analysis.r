da02bb949ab664e73cb4b000e67e08f22a94e6a9
Tobias Jansen <tobias.jansen@sap.com>
2022-10-09 18:27:26 +0200

    Cleanup

--- Tensorflow-version/analysis.r
+++ Tensorflow-version/analysis.r
@@ -1,9 +1,3 @@
-install.packages("tidyverse")
-install.packages("psych")
-library(tidyverse)
-library(ggplot2)
-library(psych)
-
 data <- read.csv("outputdata.csv")
 
 mean(data$trans_after[data$prediction_using_neural_network > 0.3])
@@ -11,8 +5,9 @@ mean(data$trans_after)
 
 
 
-# Extratest: wir können schauen, ob wenn die Gruppe mit Coupon höher predicted wird, auch der tatsächliche Kauf höher ist (und umgekehrt)
+# Additional test:
 mean(data$trans_after[data$test_coupon == 0 & data$prediction_using_neural_network > 0.3])
 mean(data$trans_after[data$test_coupon == 1 & data$prediction_using_neural_network > 0.3])
 mean(data$trans_after[data$test_coupon == 0 & data$prediction_using_neural_network < 0.3])
 mean(data$trans_after[data$test_coupon == 1 & data$prediction_using_neural_network < 0.3])
+
